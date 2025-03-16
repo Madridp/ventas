ed609c6ad43a4e0a3e0eb6367c67003bc414e831
Alexis Leon Reyes <83489240+ToshioDev@users.noreply.github.com>
2025-03-16 00:54:10 -0600

    update create_gastos_table.sql for gastos implement

--- app/database/create_gastos_table.sql
+++ app/database/create_gastos_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE gastos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
     razon VARCHAR(255) NOT NULL,
-    monto DECIMAL(10, 2) NOT NULL,
+    monto DECIMAL(10,2) NOT NULL,
     fondo ENUM('Efectivo', 'Transferencia', 'Tarjeta') NOT NULL,
-    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    caja_id INT(11)
 );
